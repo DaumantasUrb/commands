fc3f3fc5c0bd4d46ca50e684e37ed3e14b435ed7
Daumantas <steko.segmentas@gmail.com>
2020-05-17 22:05:43 +0300

    add phpstorm shortcut and window activate

--- ActivateOrLaunch.sh
+++ ActivateOrLaunch.sh
@@ -1,6 +1,12 @@
 #Open active google-chrome window or open chrome
 #todo change "sleep 0.3" to "wait till window is active"
-pid=$(pgrep ${1} | head -n 1)
+process=${1}
+if [ $process = "phpstorm" ]; then
+   process="java"
+fi
+
+pid=$(pgrep $process | head -n 1)
+
 if [ -z "$pid" ]
 then
    if [ $1 = "chrome" ]; then
@@ -13,10 +19,14 @@ then
    $1 &
    exit 1
 fi
+
 wid=$(xdotool search --onlyvisible --pid $pid 2>/dev/null | head -n 1)
 if [ -z "$wid" ]; then
    exit 2
 fi
+
+echo "window $wid"
+
 xdotool windowactivate $wid
 if [ $1 = "chrome" ]; then
    sleep 0.3s
